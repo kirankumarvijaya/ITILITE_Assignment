cdacbc0ca0a9aef2ea0b0fd0cd1133cb715c15b3
kirankumar <kirankumarvijaya@gmail.com>
2019-08-25 19:18:04 +0530

    Made adjustments for alignment in android users.

--- components/Calendar/index.js
+++ components/Calendar/index.js
@@ -30,7 +30,7 @@ export default class CalendarComponent extends React.Component {
                 hideDayNames={true}
                 
                 hideExtraDays={false}
-                style={{ width:'95%', height: height * 0.6 }}
+                style={{ width:'95%', height: height * 0.58 }}
                 // Callback which gets executed when visible months change in scroll view. Default = undefined
                 onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
                 // Max amount of months allowed to scroll to the past. Default = 50
